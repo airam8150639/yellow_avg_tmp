3530570fa1a5f1ebc95b56c1355060d5f2a0de10
Анисимова Мария <mar-anisimovaa@yandex.ru>
2021-08-26 11:08:43 +0300

    Initial commit

---
+++ yellow_avg_tmp/yellow_avg_tmp.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if (N < 0) {
+        cout << '0' << endl;
+        return 0;
+    }
+    vector<int> tmp(N);
+    int64_t sum = 0;
+
+    for (auto& t : tmp) {
+        cin >> t;
+        sum += t;
+    }
+
+    int64_t avg = (N > 0) ? sum / static_cast<int64_t>(tmp.size()) : 0;
+
+    int K = 0;
+    vector<int> result;
+    for (int i = 0; i < static_cast<int>(tmp.size()); ++i) {
+        if (tmp[i] > static_cast<int>(avg)) {
+            ++K;
+            result.push_back(i);
+        }
+    }
+
+    cout << K << endl;
+    for (auto r : result) cout << r << ' ';
+
+    return 0;
+}
